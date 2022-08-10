8b4dba03fc6d5325471d0ed588a9f9be74b894a7
Priyanka-sharma181 <Priyanka-sharma181>
2022-08-10 20:06:56 +0530

    templating

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express")
-const path = require("path")
 const app = express()
+const path = require("path")
+
 const publicDirectory = path.join(__dirname, '../public')
-console.log(publicDirectory);
+const viewsPath = path.join(__dirname,'../views')
+// app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, viewsPath));
+
 app.use(express.static(publicDirectory))
- 
+app.get("",(req,res)=>{
+    res.render('index')
+})
 app.get("/weather",(req,res)=>{
     res.send("weather")
 })
