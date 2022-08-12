f4f5d813811225ae8d57065d9c0504fea88e6196
Priyanka-sharma181 <Priyanka-sharma181>
2022-08-12 16:46:35 +0530

    custamize views directosry

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const hbs = require("hbs")
 
-const publicDirectory = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../views')
-// app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, viewsPath));
+const publicPath = path.join(__dirname, '../public')
+const temletePath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname,'../templates/partials')
+// set handelbars engine and views location 
+app.set('view engine', 'hbs');
+app.set('views', temletePath);
+hbs.registerPartials(partialPath)
 
-app.use(express.static(publicDirectory))
+
+app.use(express.static(publicPath))
 app.get("",(req,res)=>{
-    res.render('index')
+    res.render('index',{
+        Title :"weather",
+        name:"priyanka"
+    })
+})
+app.get('/about',(req,res)=>{
+  res.render('about',{
+    Title:"About me",
+    name:"priyanka sharma"
+  })
+})
+app.get('/help',(req,res)=>{
+  res.render('help',{
+    msg:"This the some help text",
+    Title:"Help",
+    name:"priyanka",
+  })
 })
 app.get("/weather",(req,res)=>{
     res.send("weather")
